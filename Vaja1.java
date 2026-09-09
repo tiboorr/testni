489e0432eba866e7af80e0822cf9c77a8c102189
tiborp <tiborp@t-2.si>
2026-09-09 09:19:39 +0200

    vaja rekurzija

--- Vaja1.java
+++ Vaja1.java
@@ -1,5 +1,20 @@
 public class Vaja1{
     public static void main(String[] args) {
-        System.out.println("Rad imam Javo");
+        System.out.println("2 na 10 = "+potenca(10));
+        System.out.println("7 fibb št je "+fib(7));
+    }
+
+    public static int potenca(int n){
+        //izračuna 2 na n rekurzivno
+        if (n == 0)
+            return 1;
+        return 2*potenca(n-1);
+    }
+
+    public static int fib(int n){
+        //izračunam nto figunacijevo st.
+        if (n == 1 || n == 2)
+            return 1;
+        return fib(n-1) + fib(n-2);
     }
 }
